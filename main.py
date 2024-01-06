d41e9d36032146b2979b5f8d18cecd8284640506
Liam McNamara <liamsmcnamara@outlook.com>
2024-01-06 16:11:54 +1100

    update

--- main.py
+++ main.py
@@ -2,17 +2,21 @@ import discord
 import os
 
 from dotenv import load_dotenv
-from datetime import datetime, timedelta, timezone, time
+from datetime import datetime, timedelta, timezone, time,date
+import pytz
 from discord.ext import tasks
 
 # Set the time of day for the message count to run.
 utc = timezone.utc
 # run_at_time = time(hour=10, minute=0, tzinfo=utc)
-run_at_time = time(hour=12, minute=21, tzinfo=utc)
+run_at_time = time(hour=10, minute=0, tzinfo=utc)
 
 # Retrieve Discord App Token from environment variable
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+
+eastern_tz = pytz.timezone('Australia/Sydney')
+hrs_diff = int(datetime.now(pytz.timezone('Australia/Sydney')).strftime('%z').split('00')[0].split('+')[1])
 
 # Declare intents
 intents=discord.Intents.default()
@@ -101,5 +105,15 @@ async def daily_channel_message_count():
 
             # Send to general chat
         await general_channel.send(announcement)
+
+# Command to inform when it will happen
+@client.event
+async def on_message(message):
+    
+    if message.author == client.user:
+        return
+    
+    if message.content == '$activity':
+        await message.channel.send("I go off at "+str((datetime.combine(date.today(), run_at_time) + timedelta(hours=hrs_diff)).strftime("%H:%M:%S %d/%m/%Y")))
 
 client.run(TOKEN)
