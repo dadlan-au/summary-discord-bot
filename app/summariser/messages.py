c348266cb5b6a552ee0ffd541df721828e864e68
l34rn3d <57818411+l34rn3d@users.noreply.github.com>
2026-05-26 23:21:20 +1000

    Update model version in num_tokens_from_messages

--- app/summariser/messages.py
+++ app/summariser/messages.py
@@ -25,7 +25,7 @@ def format_messages_for_summary(messages: List[Dict]) -> str:
     return concatenated_messages
 
 
-def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613") -> int:
+def num_tokens_from_messages(messages, model="gpt-4o-mini") -> int:
     """
     Return the number of tokens used by a list of messages.
     """
